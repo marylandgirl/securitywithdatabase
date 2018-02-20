0002444ade6bc85a077160b6613c5b5b2e170177
tizazu <esyan4>
2018-02-20 17:07:32 -0500

    Initial commit

---
+++ src/main/java/com/example/securitywithdatabase/SecuritywithdatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.example.securitywithdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritywithdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritywithdatabaseApplication.class, args);
+	}
+}
